e8a2239b1b6bbdea39332a11c388ae4796b30ce3
shristipokharel55 <shristipokharel889@gmail.com>
2025-07-17 10:06:28 +0545

    Setting route for login, register, home, verify-otp, forgot-password and reset-password page

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -2,7 +2,21 @@
 
 function App() {
 
-  return <>Hello</>
+  return(
+    <BrowserRouter>
+    <p>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/verify-otp" element={<VerifyOtp />}/>
+        <Route path="/forgot-password" element={<ForgotPassword />}/>
+        <Route path="/reset-password" element={<ResetPassword />}/>
+      </Routes>
+    </p>
+    
+    </BrowserRouter>
+  )
 }
 
 export default App
