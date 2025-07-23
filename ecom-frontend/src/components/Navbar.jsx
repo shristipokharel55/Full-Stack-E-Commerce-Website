3a1e5d7ed3f83fdf5a15a146c3ec1ac3adca9d47
shristipokharel55 <shristipokharel889@gmail.com>
2025-07-23 11:35:19 +0545

    updated nav bar section using location

--- ecom-frontend/src/components/Navbar.jsx
+++ ecom-frontend/src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { navbarFields } from "../config/navbarFields";
 
 const Navbar = () => {
+
+  const location = useLocation()
+  const pathname = location.pathname;
   return (
     <nav className="bg-white text-purple-700 shadow-md px-6 py-3 flex flex-wrap items-center justify-center gap-4 rounded-b-xl border-b border-purple-200">
-      <Link to="/" className="hover:text-pink-500 transition-colors duration-300 font-semibold">Home</Link>
-      <Link to="/login" className="hover:text-pink-500 transition-colors duration-300 font-semibold">Login</Link>
-      <Link to="/register" className="hover:text-pink-500 transition-colors duration-300 font-semibold">Register</Link>
-      <Link to="/verify-otp" className="hover:text-pink-500 transition-colors duration-300 font-semibold">Verify OTP</Link>
-      <Link to="/forgot-password" className="hover:text-pink-500 transition-colors duration-300 font-semibold">Forgot Password</Link>
-      <Link to="/reset-password" className="hover:text-pink-500 transition-colors duration-300 font-semibold">Reset Password</Link>
+      
+{
+  navbarFields.map(({ name, path }) => (
+        <Link
+          key={name}
+          
+          
+          to={path}
+          className={`${pathname===path?"font-bold bg-gray-300 p-2 rounded hover:text-blue-500":"hover:text-red-500"}`}        >
+          {name}
+        </Link>
+      )
+        )
+}
     </nav>
   );
 };
---
+++ ecom-frontend/src/config/navbarFields.js
@@ -0,0 +1,27 @@
+export const navbarFields = [{
+    name:"Home",
+    path:"/"
+},
+{
+    name:"Login",
+    path: "/login"}
+,{
+    name:"Register",
+
+    path: "/register"
+},
+{
+    name:"Verify OTP",
+    path: "/verify-otp"
+},
+{
+    name:"Forgot Password",
+    path: "/forgot-password"
+},
+{
+    name:"Reset Password",
+    path
+: "/reset-password"
+}
+
+];
