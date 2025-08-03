c5100af8ea251e514284a48affff5206c3a730fa
shristipokharel55 <shristipokharel889@gmail.com>
2025-08-03 10:24:55 +0545

    Adding controller for createOrder, getOrderById, getOrderByUserId, updateOrderStatus, updatePaymentStatus for payment process

--- ecom/src/controllers/orderController.js
+++ ecom/src/controllers/orderController.js
@@ -20,18 +20,60 @@ const createOrder = async(req, res)=>{
     }
 }
 
-const getOrderById = async (req, res) => {
+const getOrderById = async(req, res)=>{
     try {
-        const userId = req.user.id;
-        const data = await orderServices.getOrderByUserId(userId);
-
-        res.status(200).json({
-            message: "User's Order fetched successfully",
-        })
+        const id = req.params.id
+        const data = await orderServices.getOrderById(id)
+        res.status(200).json({data})
+        
     } catch (error) {
-        console.log(error.message);
-        res.status(400).send({error:error.message, message:"Error occurred while fetching order by ID"});
+        res.status(400).json({
+            message:"error to fetch the current order",
+            error:error.message
+        })
     }
 }
 
-export { createOrder, getOrderById };
+const getOrderByUserId = async (req, res) => {
+  try {
+      const userId = req.user.id
+      const data = await orderService.getOrderByUserId(userId)
+      res.status(200).json({
+        message : "User's Order Fetchec successfully",
+        data
+      })
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json ({error:error.message,message: "Error to get Order"});
+
+  }
+}
+
+const updateOrderStatus=async(req, res)=>{
+    try {
+        const orderId = req.params.id
+        const status = req.body.updateOrderStatus
+        const data = await orderServices.updateOrderStatus(orderId, status)
+    } catch (error) {
+        res.status(400).json({
+            error:error.message
+        })
+        
+    }
+}
+
+const updatePaymentStatus= async(req, res)=>{
+    try {
+        const status = req.body.updatePaymentStatus 
+        const id = req.params.id
+        const data = await orderServices.updatePaymentStatus(id, status)
+        res.status(200).json({data})
+    } catch (error) {
+        res.status(400).json({
+            message:"failed to update payment status",
+            error:error.message
+        })
+    }
+}
+
+export { createOrder, getOrderById, getOrderByUserId, updateOrderStatus, updatePaymentStatus };
